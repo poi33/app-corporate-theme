0b114eee4e58a565f11ba117372c567036ab41c2
Persijn Kwekkeboom <pkw@enonic.com>
2020-02-03 17:12:06 +0100

    fixes and new banner part

---
+++ src/main/resources/site/parts/banner-frontpage/banner-frontpage.js
@@ -0,0 +1,73 @@
+var libs = {
+    portal: require('/lib/xp/portal'),
+    thymeleaf: require('/lib/thymeleaf'),
+    content: require('/lib/xp/content'),
+    util: require('/lib/util')
+};
+
+var view = resolve('banner-frontpage.html');
+
+exports.get = function (req) {
+
+    var component = libs.portal.getComponent();
+    var banners = [];
+    var configBanners = component.config.banner ? libs.util.data.forceArray(component.config.banner) : null;
+
+    if (configBanners) {
+        for (var i = 0; i < configBanners.length; i++) {
+            var image, imageKey;
+            var hit = configBanners[i];
+            if (hit.image) {
+                imageKey = libs.content.get({
+                    key: hit.image
+                });
+            }
+            if (imageKey) {
+                image = libs.portal.imageUrl({
+                    id: imageKey._id,
+                    scale: 'width(600)'
+                });
+            }
+            var linkTarget;
+            if (hit.linkTo) {
+                linkTarget = libs.portal.pageUrl({
+                    id: hit.linkTo
+                });
+            }
+            var linkName = "Learn more";
+            if (hit.linkName)
+                linkName = hit.linkName;
+
+            var result = {
+                image: image,
+                title: hit.title1,
+                secondTitle: hit.title2,
+                linkTarget: linkTarget,
+                linkName: linkName,
+                backgroundColor: hit.backgroundColor
+            };
+
+            banners.push(result);
+        }
+    }
+
+    let slickCarousel = libs.portal.assetUrl({ path: "js/slick.min.js" });
+    let slickCss = libs.portal.assetUrl({ path: "css/slick.css" });
+
+    var model = {
+        banners,
+    };
+
+    var body = libs.thymeleaf.render(view, model);
+    return {
+        body: body,
+        pageContributions: {
+            headEnd: [
+                "<link rel='stylesheet' href='" + slickCss + "'></link>"
+            ],
+            bodyEnd: [
+                "<script src='" + slickCarousel + "'></script>"
+            ]
+        }
+    };
+};
